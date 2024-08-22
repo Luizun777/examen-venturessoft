94d783f4cf3527bb1743b99c40d23be8d3a7ab38
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 17:35:17 -0600

    Se crean componentes de layout con sus rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./layouts/login/login.component'),
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./layouts/home/home.component'),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./layouts/not-found/not-found.component'),
+  },
+];
