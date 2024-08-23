e3f98b2a96e3e2e6dcdd84243d70c02e2a700492
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 21:55:14 -0600

    Se agrega loading

--- src/app/features/menu/menu.component.ts
+++ src/app/features/menu/menu.component.ts
@@ -5,11 +5,12 @@ import { AlertService } from '@core/services/alert.service';
 import { CatalogosService } from '@core/services/catalogos.service';
 import { environment } from '@environment/environment';
 import { MenuItemComponent } from '@shared/components/menu-item/menu-item.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MenuItemComponent],
+  imports: [MenuItemComponent, MatProgressBarModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
@@ -18,12 +19,15 @@ export class MenuComponent implements OnInit {
   private alertSrv = inject(AlertService);
 
   menuItems: Categorias[] = [];
+  cargando: boolean = true;
 
   ngOnInit(): void {
     this.getCategorias();
   }
 
   getCategorias(): void {
+    this.menuItems = []
+    this.cargando = true;
     this.catalogosSrv.getCategorias().subscribe(
       ({ error, codigo, message, menuItems }: HttpResponse<Categorias[]>) => {
         if (error) {
@@ -31,9 +35,11 @@ export class MenuComponent implements OnInit {
         }
         if (menuItems.length > 0 && !this.validIdMenuDefult) {
           this.updateIdMenuDefult(menuItems[0].idMenu);
+          this.catalogosSrv.changeCategoria.emit();
         }
         this.menuItems = menuItems;
         this.updateIdMenu();
+        this.cargando = false;
       },
       () => this.alertSrv.alertError('Se ha producido un error al cargar datos')
     );
