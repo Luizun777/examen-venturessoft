26eb8a5fef73cc21952a5bc94fa56fb92c946797
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 22:04:34 -0600

    Se maqueta y se cargan valores

--- src/app/features/marcas/marcas.component.ts
+++ src/app/features/marcas/marcas.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpResponse } from '@core/interfaces/http.interface';
+import { Marcas } from '@core/interfaces/marcas.interface';
+import { AlertService } from '@core/services/alert.service';
+import { CatalogosService } from '@core/services/catalogos.service';
+import { MarcasService } from '@core/services/marcas.service';
+import { environment } from '@environment/environment';
+import { MarcaComponent } from '@shared/components/marca/marca.component';
+import { Subscription } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-marcas',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MarcaComponent,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './marcas.component.html',
-  styleUrl: './marcas.component.scss'
+  styleUrl: './marcas.component.scss',
 })
-export class MarcasComponent {
+export class MarcasComponent implements OnInit, OnDestroy {
+  private marcasSrv = inject(MarcasService);
+  private catalogosSrv = inject(CatalogosService);
+  private alertSrv = inject(AlertService);
 
+  marcas: Marcas[] = [];
+  cargando: boolean = true;
+
+  private marcasSub: Subscription = new Subscription();
+  private listenSub: Subscription = new Subscription();
+
+  ngOnInit(): void {
+    if (Boolean(localStorage.getItem(environment.idMenuDefult))) {
+      this.getMarcas();
+    }
+    this.listenCategoria();
+  }
+
+  ngOnDestroy(): void {
+    this.marcasSub.unsubscribe();
+    this.listenSub.unsubscribe();
+  }
+
+  getMarcas(): void {
+    this.marcas = [];
+    this.cargando = true;
+    this.marcasSub = this.marcasSrv.getMarcas().subscribe(
+      ({ codigo, menuItems, error, message }: HttpResponse<Marcas[]>) => {
+        if (error) {
+          this.alertSrv.alertError(`${message} ${codigo}`);
+        }
+        this.marcas = menuItems;
+        this.cargando = false;
+      },
+      () => {
+        this.alertSrv.alertError('Se ha producido un error al cargar datos');
+        this.cargando = false;
+      }
+    );
+  }
+
+  listenCategoria(): void {
+    this.listenSub = this.catalogosSrv.changeCategoria.subscribe(() => {
+      this.marcasSub.unsubscribe();
+      this.getMarcas();
+    });
+  }
 }
