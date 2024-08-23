b753d97d31315f722a8b08e4f4c3fbc3d578fd80
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 19:00:57 -0600

    Se ajsuta estilo de header y footer  para home

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatSelectModule, MatIconModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
