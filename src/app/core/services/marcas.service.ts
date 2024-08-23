8f6472a8de668bcaeb8d53327586fe872ae51632
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 22:00:55 -0600

    Se agrega servicio para marcas

--- src/app/core/services/marcas.service.ts
+++ src/app/core/services/marcas.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { MapingService } from './maping.service';
+import { map, Observable } from 'rxjs';
+import { Marcas } from '@core/interfaces/marcas.interface';
+import { environment } from '@environment/environment';
+import { HttpResponse } from '@core/interfaces/http.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MarcasService {
+  private http = inject(HttpClient);
+  private mapingSrv = inject(MapingService);
 
-  constructor() { }
+  getMarcas(): Observable<HttpResponse<Marcas[]>> {
+    const idMenu = localStorage.getItem(environment.idMenuDefult) ?? 0;
+    return this.http
+      .get<HttpResponse<Marcas[]>>(`${environment.apiServer}Marcas?idMenu=${idMenu}`)
+      .pipe(
+        map((response: HttpResponse<Marcas[]>) => ({
+          ...response,
+          menuItems: this.mapingSrv.acentoDescripcion(response.menuItems),
+        }))
+      );
+  }
 }
