8e4981dfe4d25f5b1af80d95c94efc63c7e89d1d
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 20:31:24 -0600

    Se ajusta home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MenuComponent } from "@shared/components/menu/menu.component";
 
 @Component({
   selector: 'featur-home',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
