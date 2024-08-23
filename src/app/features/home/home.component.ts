45c0dc99ca868fa1dc8a0c0739d2c8c3dd32310d
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 19:10:13 -0600

    Se ajustan componentes home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "@shared/components/menu/menu.component";
 
 @Component({
-  selector: 'app-home',
+  selector: 'featur-home',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "@shared/components/header/header.component";
-import { FooterComponent } from "@shared/components/footer/footer.component";
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { FooterComponent } from '@shared/components/footer/footer.component';
+import { HomeComponent as FeaturesHomeComponent } from '@features/home/home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FeaturesHomeComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export default class HomeComponent {
-
-}
+export default class HomeComponent {}
