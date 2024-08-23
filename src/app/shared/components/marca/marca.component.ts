eccc41edd9b7340f4fbe04c9c4e3cfaa93b6b8ed
Luis Acosta <luisacosta360@gmail.com>
2024-08-22 23:22:35 -0600

    Se maqueta y carga informacion en marca

--- src/app/shared/components/marca/marca.component.ts
+++ src/app/shared/components/marca/marca.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Marcas } from '@core/interfaces/marcas.interface';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-marca',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './marca.component.html',
-  styleUrl: './marca.component.scss'
+  styleUrl: './marca.component.scss',
 })
 export class MarcaComponent {
-
+  @Input() marca: Marcas = {} as Marcas;
 }
