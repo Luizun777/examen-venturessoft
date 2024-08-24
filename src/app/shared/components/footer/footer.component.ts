a9c2e0c5b1805d67efecf9ce42204d32877b7898
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 22:57:11 -0600

    Se agrega cambio de lenguaje en footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { MatSelectModule } from '@angular/material/select';
+import { Component, inject } from '@angular/core';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
+import { TranslationService } from '@core/services/translation.service';
+import { CatLanguage } from '@shared/catalogos/CatLanguage';
+import { CatalogGeneric } from '@core/interfaces/catalogo-generico.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatSelectModule, MatIconModule],
+  imports: [CommonModule, MatSelectModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public translationSrv = inject(TranslationService);
+
+  catLanguage: CatalogGeneric[] = CatLanguage;
+
+  async loadLanguage(language: string) {
+    await this.translationSrv.loadTranslations(language);
+  }
+
+  selectLanguage({ value }: MatSelectChange): void {
+    this.loadLanguage(value);
+  }
+}
