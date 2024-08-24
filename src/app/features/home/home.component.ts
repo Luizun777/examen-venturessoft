6870ea5e88ad24fd1669348fe0f62b20afb28765
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 18:47:42 -0600

    Se importa componnete de deatlles

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MenuComponent } from '../menu/menu.component';
 import { MarcasComponent } from "../marcas/marcas.component";
+import { DetailsComponent } from "../details/details.component";
 
 @Component({
   selector: 'featur-home',
   standalone: true,
-  imports: [MenuComponent, MatButtonModule, MarcasComponent],
+  imports: [MenuComponent, MatButtonModule, MarcasComponent, DetailsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
