711b8e1fff2ae6b54d297f0e77f3095464432b32
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 18:24:19 -0600

    Se crea compponente detalle

--- src/app/shared/components/detail/detail.component.ts
+++ src/app/shared/components/detail/detail.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Marcas } from '@core/interfaces/marcas.interface';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, MatTooltipModule],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  styleUrl: './detail.component.scss',
 })
 export class DetailComponent {
-
+  @Input() marca: Marcas = {} as Marcas;
 }
