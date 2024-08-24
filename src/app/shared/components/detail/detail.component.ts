c7d9cdc08106f387bbac6e45990b3f555a75bad5
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 19:55:39 -0600

    Se elmina componente marca

--- src/app/shared/components/detail/detail.component.ts
+++ src/app/shared/components/detail/detail.component.ts
@@ -4,14 +4,32 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Marca } from '@core/interfaces/marcas.interface';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { BtnTextIconComponent } from "../btn-text-icon/btn-text-icon.component";
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatIconModule, MatTooltipModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, MatTooltipModule, BtnTextIconComponent],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss',
 })
 export class DetailComponent {
   @Input() marca: Marca = {} as Marca;
+  @Input() isCashback: boolean = true;
+
+  get background(): string | undefined {
+    return this.isCashback ? this.marca.background : this.marca.imagen;
+  }
+
+  get badgeClass(): string {
+    return this.isCashback ? ' card-badge' : ' coupon-badge';
+  }
+
+  get badgeTitle(): string {
+    return this.isCashback ? 'Cashback' : 'Instant Coupon';
+  }
+
+  get badgeIcon(): string {
+    return this.isCashback ? 'local_atm' : 'card_travel';
+  }
 }
