873d3d0beb4915fce462e153d571d7aac83e4523
Luis Acosta <luisacosta360@gmail.com>
2024-08-24 17:07:14 -0600

    Se elimima carpeta docs

--- docs/chunk-F7NLQDVY.js
+++
@@ -1,1 +0,0 @@
-import{Gb as r,Kb as s,fa as o,rb as i,sb as a}from"./chunk-Y2LLFJTV.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-login"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"login works!"),a())}});let t=e;return t})();export{d as default};
