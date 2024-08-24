2d409fb7fcf7e20a45fcc5fd3013a9c3355a0c36
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 18:36:46 -0600

    Se agrega catalogo de imagens

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,10 @@
 export const environment = {
   apiServer: 'https://eland-dk.humaneland.net/Examen/AngularApi/',
   idMenuDefult: 'idMenu',
+  catUnlock: [
+    'assets/images/details/gas.webp',
+    'assets/images/details/hotel.webp',
+    'assets/images/details/food.webp',
+    'assets/images/details/dev.webp',
+  ],
 };
