fd18003eea897eb2894279e7aebf3cf6b0ea5ed4
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 20:23:27 -0600

    Se crea componente detalle row

---
+++ src/app/shared/components/detail-row/detail-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-row.component.html',
+  styleUrl: './detail-row.component.scss'
+})
+export class DetailRowComponent {
+
+}
