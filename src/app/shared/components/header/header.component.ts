159570012e146beda711996d51dcd60f0db19e31
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 22:58:49 -0600

    Se agrega el primer cambio de lenguaje

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { BtnTextIconComponent } from '../btn-text-icon/btn-text-icon.component';
+import { Observable } from 'rxjs';
+import { TranslationService } from '@core/services/translation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [BtnTextIconComponent],
+  imports: [CommonModule, BtnTextIconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  private translationSrgv = inject(TranslationService);
+
+  header$: Observable<{ [key: string]: string }> = new Observable();
+
+  ngOnInit(): void {
+    this.header$ = this.translationSrgv.getTranslationObject$('header');
+  }
+}
