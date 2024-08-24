0f10ac126f522a0b68bd9316c6bd4246e5c5e26c
Luis Acosta <luisacosta360@gmail.com>
2024-08-23 18:47:14 -0600

    Se consume servicio para agregar cashback

---
+++ src/app/features/details/details.component.ts
@@ -0,0 +1,58 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { HttpResponse } from '@core/interfaces/http.interface';
+import { Marca } from '@core/interfaces/marcas.interface';
+import { MarcasService } from '@core/services/marcas.service';
+import { environment } from '@environment/environment';
+import { DetailComponent } from '@shared/components/detail/detail.component';
+import { forkJoin, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, DetailComponent],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.scss',
+})
+export class DetailsComponent implements OnInit, OnDestroy {
+  private marcasSrv = inject(MarcasService);
+
+  marcas: Marca[] = [];
+
+  private marcasSub: Subscription = new Subscription();
+
+  ngOnInit(): void {
+    this.getMarcas();
+  }
+
+  ngOnDestroy(): void {
+    this.marcasSub.unsubscribe();
+  }
+
+  getMarcas(): void {
+    this.marcasSub = forkJoin([
+      this.marcasSrv.getMarcas(4600), // Fuel
+      this.marcasSrv.getMarcas(3400), // Travel
+      this.marcasSrv.getMarcas(2100), // Restaurant
+      this.marcasSrv.getMarcas(1100), // Softwere
+    ]).subscribe((marcas: HttpResponse<Marca[]>[]) => {
+      this.marcas = marcas
+        .map(({ menuItems, error }: HttpResponse<Marca[]>, index: number) => {
+          if (error) {
+            return undefined;
+          }
+          return <Marca>{
+            ...this.marcaRandom(menuItems),
+            background: environment.catUnlock[index],
+          };
+        })
+        .filter((marca: Marca | undefined) => marca) as Marca[];
+    });
+  }
+
+  marcaRandom(marcas: Marca[]): Marca {
+    const indiceAleatorio = Math.floor(Math.random() * marcas.length);
+    return marcas[indiceAleatorio];
+  }
+}
