10733b6906a2fcaee430204594e604239b8154ce
Luis Acosta <luisacosta360@gmail.com>
2024-08-24 13:57:50 -0600

    Se agrega fiuncionalidat y maqueta en detallerow

--- src/app/shared/components/detail-row/detail-row.component.ts
+++ src/app/shared/components/detail-row/detail-row.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BtnTextIconComponent } from '../btn-text-icon/btn-text-icon.component';
+import { MatListModule } from '@angular/material/list';
+import { Marca } from '@core/interfaces/marcas.interface';
+import { TranslationService } from '@core/services/translation.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-detail-row',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatTooltipModule,
+    BtnTextIconComponent,
+    MatListModule,
+  ],
   templateUrl: './detail-row.component.html',
-  styleUrl: './detail-row.component.scss'
+  styleUrl: './detail-row.component.scss',
 })
-export class DetailRowComponent {
+export class DetailRowComponent implements OnInit, OnDestroy {
+  private translationSrv = inject(TranslationService);
 
+  @Input() marca: Marca = {} as Marca;
+
+  private translationSub: Subscription = new Subscription();
+  translations: any = {};
+
+  ngOnInit(): void {
+    this.translationSub = this.translationSrv.translations.subscribe(
+      (translations) => (this.translations = translations['detail'])
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.translationSub.unsubscribe();
+  }
 }
