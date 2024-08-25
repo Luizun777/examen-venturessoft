d8694064d2e3eb22725e4fb73460116390cf4fa0
Luis Acosta <luisacosta360@gmail.com>
2024-08-25 00:10:51 -0600

    Se crea componnete de tabla

--- ventures-soft/src/app/features/features.module.ts
+++ ventures-soft/src/app/features/features.module.ts
@@ -4,10 +4,21 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { FeaturesHomeComponent } from './features-home/features-home.component';
 import { TextColorPipe } from '@shared/pipe/text-color.pipe';
+import { MaterialsModule } from '@core/materials/materials.module';
+import { TablaComponent } from './tabla/tabla.component';
+
+const componets = [FeaturesHomeComponent, TablaComponent];
 
 @NgModule({
-  declarations: [FeaturesHomeComponent],
-  exports: [FeaturesHomeComponent],
-  imports: [IonicModule, CommonModule, ReactiveFormsModule, FormsModule, TextColorPipe],
+  declarations: componets,
+  exports: componets,
+  imports: [
+    IonicModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TextColorPipe,
+    MaterialsModule,
+  ],
 })
 export class FeaturesModule {}
---
+++ ventures-soft/src/app/features/tabla/tabla.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { TableCard } from '@core/interfaces/table-card.interface';
+import { TranslationService } from '@core/services/translation.service';
+import { Subscription } from 'rxjs';
+import {
+  CatTablaEn,
+  CatTablaEs,
+  CatTablaHeader,
+} from 'src/assets/catalogos/CatTable';
+
+@Component({
+  selector: 'app-tabla',
+  templateUrl: './tabla.component.html',
+  styleUrls: ['./tabla.component.scss'],
+})
+export class TablaComponent implements OnInit, OnDestroy {
+  private translationSrv = inject(TranslationService);
+
+  displayedColumns: string[] = CatTablaHeader;
+  dataSource: TableCard[] = [];
+
+  private translationSub: Subscription = new Subscription();
+  translations: any = {};
+
+  ngOnInit(): void {
+    this.translationSub = this.translationSrv.translations.subscribe(
+      (translations: any) => {
+        this.translations = translations['tabla'];
+        this.dataSource =
+          translations['languiaje'] === 'es' ? CatTablaEs : CatTablaEn;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.translationSub.unsubscribe();
+  }
+}
