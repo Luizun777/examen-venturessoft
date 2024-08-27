02ae6c0cc5db00eb7f2c8d9261562c02d0687460
Luis Acosta <luisacosta360@gmail.com>
2024-08-27 13:36:46 -0600

    Se agregan categorias para cargar catalogos random

--- src/app/features/details/details.component.ts
+++ src/app/features/details/details.component.ts
@@ -4,12 +4,13 @@ import { HttpResponse } from '@core/interfaces/http.interface';
 import { Marca } from '@core/interfaces/marcas.interface';
 import { MarcasService } from '@core/services/marcas.service';
 import { DetailComponent } from '@shared/components/detail/detail.component';
-import { firstValueFrom, forkJoin, Observable, Subscription } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
 import { BtnTextIconComponent } from '@shared/components/btn-text-icon/btn-text-icon.component';
-import { CatUnlock } from 'src/assets/catalogos/CatUnlockImages';
 import { TranslationService } from '@core/services/translation.service';
 import { AlertService } from '@core/services/alert.service';
 import { CommonModule } from '@angular/common';
+import { Categorias } from '@core/interfaces/categorias.interface';
+import { CatalogosService } from '@core/services/catalogos.service';
 
 @Component({
   selector: 'app-details',
@@ -20,54 +21,85 @@ import { CommonModule } from '@angular/common';
 })
 export class DetailsComponent implements OnInit, OnDestroy {
   private marcasSrv = inject(MarcasService);
+  private catalogosSrv = inject(CatalogosService);
   private alertSrv = inject(AlertService);
   private translationSrv = inject(TranslationService);
 
   marcas: Marca[] = [];
-  catUnlock: string[] = CatUnlock;
 
-  details$: Observable<{ [key: string]: string }> = new Observable();
+  private translationSub: Subscription = new Subscription();
+  translations: any = {};
 
   private marcasSub: Subscription = new Subscription();
+  private catalogosSub: Subscription = new Subscription();
 
   ngOnInit(): void {
-    this.details$ = this.translationSrv.getTranslationObject$('details');
-    this.getMarcas();
+    this.translationSub = this.translationSrv.translations.subscribe(
+      (translations: any) => (this.translations = translations['details'])
+    );
+    this.getCategorias();
   }
 
   ngOnDestroy(): void {
     this.marcasSub.unsubscribe();
+    this.translationSub.unsubscribe();
+    this.catalogosSub.unsubscribe();
   }
 
-  getMarcas(): void {
-    this.marcasSub = forkJoin([
-      this.marcasSrv.getMarcas(4600), // Fuel
-      this.marcasSrv.getMarcas(3400), // Travel
-      this.marcasSrv.getMarcas(2100), // Restaurant
-      this.marcasSrv.getMarcas(1100), // Softwere
-    ]).subscribe(
-      (marcas: HttpResponse<Marca[]>[]) => {
+  getCategorias(): void {
+    this.catalogosSub = this.catalogosSrv.getCategorias().subscribe({
+      next: ({
+        error,
+        codigo,
+        message,
+        menuItems,
+      }: HttpResponse<Categorias[]>) => {
+        if (error) {
+          this.alertSrv.alertError(`${message} ${codigo}`);
+        }
+        if (menuItems.length > 0) {
+          const newListCat = this.getRandomCategorias(menuItems);
+          this.getMarcas(newListCat);
+        }
+      },
+      error: () =>
+        this.alertSrv.alertError('Se ha producido un error al cargar datos'),
+    });
+  }
+
+  getMarcas(categorias: Categorias[]): void {
+    const listPeticion = categorias.map(({ idMenu }: Categorias) =>
+      this.marcasSrv.getMarcas(idMenu)
+    );
+    this.marcasSub = forkJoin(listPeticion).subscribe({
+      next: (marcas: HttpResponse<Marca[]>[]) => {
         this.marcas = marcas
           .map(({ menuItems, error }: HttpResponse<Marca[]>, index: number) => {
             if (error) {
               return undefined;
             }
+            const background = `assets/images/details/${categorias[index][
+              'descripción'
+            ].trim()}.webp`;
             return <Marca>{
               ...this.marcaRandom(menuItems),
-              background: this.catUnlock[index],
+              background,
             };
           })
           .filter((marca: Marca | undefined) => marca) as Marca[];
       },
-      async () => {
-        const mensaje = await firstValueFrom(this.details$);
-        this.alertSrv.alertError(mensaje['errorFetch']);
-      }
-    );
+      error: () => {
+        this.alertSrv.alertError(this.translations['errorFetch']);
+      },
+    });
   }
 
   marcaRandom(marcas: Marca[]): Marca {
     const indiceAleatorio = Math.floor(Math.random() * marcas.length);
     return marcas[indiceAleatorio];
+  }
+
+  getRandomCategorias(arrayCat: Categorias[]): Categorias[] {
+    return arrayCat.sort(() => 0.5 - Math.random()).slice(0, 4);
   }
 }
